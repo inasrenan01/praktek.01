9bc256ee391549f9876ca429d5ffb02ef4b7c569
inasrenan01 <xidorenan@gmail.com>
2018-03-28 22:35:43 +0800

    instance object

--- src/praktek/pkg01/semartphoneaksi.java
+++ src/praktek/pkg01/semartphoneaksi.java
@@ -9,8 +9,11 @@ package praktek.pkg01;
  *
  * @author USER
  */
-public class smartphoneaksi {
+public class semartphoneaksi {
     public static void main(String[] args) {
+        semartphone oppo = new semartphone();
+        semartphone apple = new semartphone();
+        semartphone samsung = new semartphone();
         
     }
 }
